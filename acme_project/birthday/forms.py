cf98872c2932c4ef94384076017ccdc51935e795
DenisBelikov <DenisBelikov1992@yandex.ru>
2025-02-03 14:20:27 +0300

    acme_project

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,20 +1,47 @@
 # birthday/forms.py
 from django import forms
-
-# Импортируем класс модели Birthday.
+from .validators import real_age
+from django.core.exceptions import ValidationError
 from .models import Birthday
 
+BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'}
 
-# Для использования формы с моделями меняем класс на forms.ModelForm.
-class BirthdayForm(forms.ModelForm):
-    # Удаляем все описания полей.
 
-    # Все настройки задаём в подклассе Meta.
+class BirthdayForm(forms.Form):
+    first_name = forms.CharField(label='Имя', max_length=20)
+    last_name = forms.CharField(
+        label='Фамилия', required=False, help_text='Необязательное поле'
+    )
+
     class Meta:
-        # Указываем модель, на основе которой должна строиться форма.
-        model = Birthday
-        widgets = {
-            'birthday': forms.DateInput(attrs={'type': 'date'})
-        }
-        # Указываем, что надо отобразить все поля.
-        fields = '__all__'
+        constraints = (
+            models.UniqueConstraint(
+                fields=('first_name', 'last_name', 'birthday'),
+                name='Unique person constraint',
+            ),
+        )
+
+    birthday = forms.DateField(
+        label='Дата рождения',
+        widget=forms.DateInput(attrs={'type': 'date'}),
+        # В аргументе validators указываем список или кортеж 
+        # валидаторов этого поля (валидаторов может быть несколько).
+        validators=(real_age,),
+    )
+
+    def clean_first_name(self):
+        # Получаем значение имени из словаря очищенных данных.
+        first_name = self.cleaned_data['first_name']
+        # Разбиваем полученную строку по пробелам
+        # и возвращаем только первое имя.
+        return first_name.split()[0]
+
+    def clean(self):
+        # Вызов родительского метода clean.
+        super().clean()
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        if f'{first_name} {last_name}' in BEATLES:
+            raise ValidationError(
+                'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
+            )
--- acme_project/birthday/urls.py
+++ acme_project/birthday/urls.py
@@ -8,7 +8,10 @@ app_name = 'birthday'
 
 urlpatterns = [
     path('', views.birthday, name='create'),
-]
+    path('list/', views.birthday_list, name='list'),
+    path('<int:pk>/edit/', views.birthday, name='edit'),
+    path('<int:pk>/delete/', views.delete_birthday, name='delete'),
+] 
 
 
 def calculate_birthday_countdown(birthday):
